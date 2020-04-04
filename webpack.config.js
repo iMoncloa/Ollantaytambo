8172d98960561cf5c276c8ac83645861b0900a0a
Daniel Peña <toyeca@gmail.com>
2020-04-04 21:20:34 +0200

    First empty web to start writting HTML code.

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env, options) => ({
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              config: {
+                ctx: {
+                  webpack: { ...options },
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ico$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              outputPath: (url, resourcePath, context) => {
+                return url.replace(/^src\//, "");
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
